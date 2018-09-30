23bbfb2abb5c46af3da70aa1aeb2ad1b6e256e83
silvena <silvena@umich.edu>
2018-09-29 23:24:31 -0400

    vectorizing utils and requirements for them

---
+++ app/vectorize.py
@@ -0,0 +1,43 @@
+import requests
+from PIL import Image
+from keras.preprocessing import image
+from keras.applications.mobilenet import MobileNet, preprocess_input
+from io import BytesIO
+import numpy as np
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, as_completed
+
+global model
+
+def get_images(url_dict):
+    def get(url):
+        response = requests.get(url)
+        temp = Image.open(BytesIO(response.content))
+        img = temp.copy().resize((224,224))
+        temp.close()
+        return img
+
+    results = dict()
+    with ThreadPoolExecutor(5) as executor:
+        future_to_id = {executor.submit(get, url): key for key, url in url_dict.items()}
+        for future in as_completed(future_to_id):
+            unique_id = future_to_id[future]
+            try:
+                img = future.result()
+                results[unique_id] = img
+            except Exception as e:
+                print(e)
+    return results
+
+def get_vectors(url_dict):
+    image_dict = get_images(url_dict)
+    if 'model' not in globals():
+        model = MobileNet(input_shape=(224,224,3), include_top=False, weights='imagenet', pooling='avg')
+
+    results = dict()
+    for unique_id, img in image_dict.items():
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        vector = model.predict(x)
+        results[unique_id] = vector
+    return results
