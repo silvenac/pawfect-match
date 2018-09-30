3c3cc180fb9aea456cfd388c8ddb46df8688ec28
msats5 <saterson@gmail.com>
2018-09-30 01:18:46 -0400

    added form post request

--- app/routes.py
+++ app/routes.py
@@ -3,23 +3,31 @@ from flask import request
 from app import app
 import requests
 
+API_KEY = "111"
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        print(request.form)
-        '''
         url = "http://api.petfinder.com/pet.find"
         query_string = {
-            "key": "API_KEY",
+            "key": API_KEY,
             "animal": "dog",
             "format": "json",
-            "location": "new york city, ny",
+            "location": request.form['location'],
+            "sex": request.form['sex'],
+            "age": request.form['age'],
+            "size": request.form['size'],
             "count": "25",
-            "ouput": "basic"
+            "output": "basic"
         }
         response = requests.get(url, params=query_string)
         data = response.json()['petfinder']['pets']['pet'] # array of pets 
-        print(data[0])
-        '''
+        url_dict = {}
+        for i in range(len(data)):
+            try:
+                url_dict[data[i]['id']['$t']] = data[i]['media']['photos']['photo'][0]['$t']
+            except:
+                print(i)
+        print(url_dict)
         return "hello"
     return render_template('index.html')
